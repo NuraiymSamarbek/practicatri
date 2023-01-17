9d95aa2c8d53295cd6183d477e15c800904f695b
NuraiymSamarbek <113868732+NuraiymSamarbek@users.noreply.github.com>
2023-01-17 12:40:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/practicatri/Repos.java
@@ -0,0 +1,19 @@
+package com.example.practicatri;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Repos {
+
+    @SerializedName("name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
